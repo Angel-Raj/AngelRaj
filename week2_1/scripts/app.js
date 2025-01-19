3fab43524d5865187f0dbbb142776359eb391cdb
Angel-Raj <angelraj.shrestha@dcmail.ca>
2025-01-18 23:19:00 -0500

    My Site

--- week2_1/scripts/app.js
+++ week2_1/scripts/app.js
@@ -6,3 +6,39 @@
     }
     window.addEventListener("load", Start);
 })();
+
+function DisplayHomePage(){
+
+    let AboutUsButton = document.getElementById("AboutUsBtn");
+    AboutUsButton.addEventListener("click", function()
+    {
+        location.href = "about.html";
+    });
+}
+
+function DisplayProductsPage(){
+
+    let AboutUsButton = document.getElementById("ProductsBtn");
+    AboutUsButton.addEventListener("click", function()
+    {
+        location.href = "products.html";
+    });
+}
+
+function DisplayServicesPage(){
+
+    let AboutUsButton = document.getElementById("ServicesBtn");
+    AboutUsButton.addEventListener("click", function()
+    {
+        location.href = "services.html";
+    });
+}
+
+function DisplayContactPage(){
+
+    let AboutUsButton = document.getElementById("ContactUsBtn");
+    AboutUsButton.addEventListener("click", function()
+    {
+        location.href = "contact.html";
+    });
+}
